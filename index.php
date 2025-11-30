a6e022f5a960de1b8ffa24a1f5f9420877d4ba9c
matthieuthuetdev <mthuet.pro@gmail.com>
2025-11-30 12:45:29 +0100

    correcte the file

--- index.php
+++ index.php
@@ -1,26 +1,64 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Objectifs du projets</title>
+    <title>Objectifs du projet</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-    <h1>Liste des objectifs de ce projet MyToolBox et mon avensement</h1>
+    <h1>Liste des objectifs du projet MyToolBox et mon avancement</h1>
+
+    <div class="legend">
+        <p><strong>Légende des couleurs :</strong></p>
+        <ul>
+            <li class="toDo">Tâches à faire</li>
+            <li class="inProgresse">Tâches en cours</li>
+            <li class="done">Tâches terminées</li>
+        </ul>
+    </div>
+
     <div>
         <ul>
-            <li class="done">rédiger le cahier des charges et créé les diagrammes (cas d'utilisation, diagrammes de class) <a href="./specifications.php">Voir le cahier des charges et les diagrammes</a></li>
-            <li class="done">mettre en place le déployement automatique depuis el dépot github (Toutes les 5 minutes un script php effectue un pull sur le github afin de récuperer la dernière vertion du projet sur la branche main)</li>
-            <li class="done">Choisire la bonne structure d'url a utiliser dans le rooter</li>
-            <li class="inProgresse">créé la structure du projet en MVC</li>
-            <li class="toDo">créé le style des pages publices, page d'accueil, page a propo etc...</li>
-            <li class="toDo">créé le sous dommaine pour l'application et pour la beta</li>
-            <li class="toDo">créé le back end de l'application</li>
-            <li class="toDo">Créé les premiers outils utilisables par tousses</li>
-            <li class="toDo">faire els premiers testes en avec un groupe réstrain d'utilisateurs (mes parrents et mon frère)</li>
+            <li class="done">
+                Rédaction du cahier des charges et création des diagrammes (cas d'utilisation, diagrammes de classes)
+                <a href="./specifications.php">Voir le cahier des charges et les diagrammes</a>
+            </li>
+
+            <li class="done">
+                Mise en place du déploiement automatique depuis le dépôt GitHub
+                (toutes les 5 minutes, un script PHP effectue un pull afin de récupérer la dernière version du projet sur la branche main)
+            </li>
+
+            <li class="done">
+                Choisir la bonne structure d’URL à utiliser dans le routeur
+            </li>
+
+            <li class="inProgresse">
+                Création de la structure du projet en MVC
+            </li>
+
+            <li class="toDo">
+                Création du style des pages publiques : page d’accueil, page “À propos”, etc.
+            </li>
+
+            <li class="toDo">
+                Création du sous-domaine pour l’application et pour la version bêta
+            </li>
+
+            <li class="toDo">
+                Création du back-end de l’application
+            </li>
+
+            <li class="toDo">
+                Création des premiers outils utilisables par tous
+            </li>
+
+            <li class="toDo">
+                Réalisation des premiers tests avec un groupe restreint d’utilisateurs (mes parents et mon frère)
+            </li>
         </ul>
     </div>
 </body>
