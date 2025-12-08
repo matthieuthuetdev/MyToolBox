97f6755f4ca54a9de8b9e791d0706ff6db61ff58
matthieuthuetdev <mthuet.pro@gmail.com>
2025-12-08 16:06:54 +0100

    create the visitor menu

---
+++ app/Views/menu/visitorMenu.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php?pagecontroller=home&action=home">MyToolBox</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php?pagecontroller=home&action=home">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?pagecontroller=home&action=about">À propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="index.php?pagecontroller=tool&action=display">Tous les outils</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="index.php?pagecontroller=user&action=signup">S’inscrire</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="index.php?pagecontroller=user&action=signin">Se connecter</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
